fa5d1276179028d542dc881f1658c583aaf5ea9c
Wesley Petit <wesley.petit.lemoine@gmail.com>
2023-07-09 14:51:00 +0200

    Add running animation, change animator speed based on run speed and disable animator on pause

--- Assets/Scripts/GameJamCharacter.cs
+++ Assets/Scripts/GameJamCharacter.cs
@@ -5,36 +5,55 @@ using WSWhitehouse.TagSelector;
 public class GameJamCharacter : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] Vector3 baseDirection = new(0, 0, 1);
-    [SerializeField] [Range(0f, 30f)] float speed = 1f;
-    [SerializeField] [Range(0f, 30f)] float maxSpeed = 20f;
-    [SerializeField, TagSelector] private string groundTag;
-    
+    [SerializeField, Range(0f, 30f)] float speed = 1f;
+    [SerializeField, Range(0f, 30f)] float maxSpeed = 20f;
+    [SerializeField, Range(0f, 700f)] float rotationSpeed = 20f;
+    [SerializeField, TagSelector] string groundTag;
+    [SerializeField] Animator animator;
+
     public Action onDeath;
 
-    private Rigidbody _rigidbody;
+    Transform _transform;
+    Rigidbody _rigidbody;
+    Vector3 _direction = new(0f, 0f, 1f);
 
+    float _baseSpeed;
+    
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        ChangeDirection(transform.forward);
+        _transform = GetComponent<Transform>();
+        ChangeDirection(_transform.forward);
     }
 
-    private void FixedUpdate()
+    private void Start() => _baseSpeed = speed;
+
+    private void Update() => SmoothRotateToDirection();
+
+    private void FixedUpdate() => Move();
+
+    private void OnCollisionEnter(Collision col)
     {
-        // Constant movement without ignoring gravity force
-        _rigidbody.velocity = baseDirection.normalized * speed + Vector3.up * _rigidbody.velocity.y;
+        // Do nothing if the player hit the ground after a jump
+        // or normally
+        if (col.gameObject.CompareTag(groundTag))
+            return;
+        
+        Death();
     }
-
+    
     public void ChangeDirection(in Vector3 newDirection)
     {
-        baseDirection = newDirection;
+        _direction = newDirection;
     }
 
     public void IncreaseSpeed(float addedSpeed)
     {
         speed += addedSpeed;
         speed = Mathf.Clamp(speed, 0f, maxSpeed);
+
+        // Change animation speed on increase speed
+        animator.speed = speed / _baseSpeed;
     }
 
     public void Jump(float jumpForce)
@@ -47,21 +66,13 @@ public class GameJamCharacter : MonoBehaviour
         _rigidbody.angularVelocity = Vector3.zero;
         _rigidbody.velocity = Vector3.zero;
         enabled = false;
+        animator.enabled = false;
     }
 
     public void Resume()
     {
         enabled = true;
-    }
-
-    private void OnCollisionEnter(Collision col)
-    {
-        // Do nothing if the player hit the ground after a jump
-        // or normally
-        if (col.gameObject.CompareTag(groundTag))
-            return;
-        
-        Death();
+        animator.enabled = true;
     }
 
     public void Death()
@@ -70,5 +81,21 @@ public class GameJamCharacter : MonoBehaviour
 
         onDeath?.Invoke();
         Debug.Log("Player Is Dead !");
+    }
+
+    private void Move()
+    {
+        // Constant movement without ignoring gravity force
+        _rigidbody.velocity = _direction.normalized * speed + Vector3.up * _rigidbody.velocity.y;
+    }
+
+    // Rotate the player to follow the current direction
+    private void SmoothRotateToDirection()
+    {
+        // Convert direction to rotation
+        Quaternion directionRotation = Quaternion.LookRotation(_direction, Vector3.up);
+        
+        // Smooth rotation with an interpolation value
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, directionRotation, rotationSpeed * Time.deltaTime);
     }
 }
